4332d85bf014ad494781552a68c54f0fe5b4e935
Igor Simonenko <simonenko89@gmail.com>
2017-05-03 21:51:25 +0300

    index.php 1st

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 include 'functions.php';
-
+var_dump($_POST);
 $days = rand(0, 3);
 $task_deadline_ts = strtotime("+" . $days . " day"); 
 $current_ts = time();
@@ -50,7 +50,7 @@ $tasks = [
 
 $tasks_filter = [];
 
-if (isset($_GET['project'])) {
+if (isset($_GET['project']) || (isset($_POST['send']) && !isset($_POST['name']))) {
     if (!array_key_exists($_GET['project'], $project_list)) {
         
     header("HTTP/1.1 404 Not Found");
@@ -64,71 +64,75 @@ if (isset($_GET['project'])) {
     $tasks_filter = $tasks;
 };
 
+if (isset($_FILES['preview']) && is_uploaded_file($_FILES['preview']['name'])) {
+    move_uploaded_file($_FILES['preview']['tmp_name'], 'doingsdone/');
+}
+
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Дела в Порядке!</title>
-    <link rel="stylesheet" href="css/normalize.css">
-    <link rel="stylesheet" href="css/style.css">
-</head>
+    <head>
+        <meta charset="UTF-8">
+        <title>Дела в Порядке!</title>
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="css/style.css">
+    </head>
 
-<body><!--class="overlay"-->
-<h1 class="visually-hidden">Дела в порядке</h1>
-    <?=include_template('templates/header.php'); ?>
-<div class="page-wrapper">
-    <div class="container container--with-sidebar">
-        <?=include_template('templates/main.php', ['project_list' => $project_list, 'tasks' => $tasks, 'days_until_deadline' => $days_until_deadline, 'date_deadline' => $date_deadline, 'tasks_filter' => $tasks_filter]); ?>
-    </div>
-</div>
-<?=include_template('templates/footer.php'); ?>
-    
-<div class="modal" hidden>
-    <button class="modal__close" type="button" name="button">Закрыть</button>
+    <?php if (isset($_GET['add']) || strlen($_POST['name']) == 0 || strlen($_POST['project']) == 0 || strlen($_POST['date']) == 0): ?>
+    <body class=<?="overlay";?> >
+    <?= include_template('form.php', ['project_list' => $project_list]); ?>
+    <?php endif; ?>
 
-    <h2 class="modal__heading">Добавление задачи</h2>
-
-    <form class="form" class="" action="index.html" method="post">
-        <div class="form__row">
-            <label class="form__label" for="name">Название <sup>*</sup></label>
-
-            <input class="form__input" type="text" name="name" id="name" value="" placeholder="Введите название">
-        </div>
-
-        <div class="form__row">
-            <label class="form__label" for="project">Проект <sup>*</sup></label>
-
-            <select class="form__input form__input--select" name="project" id="project">
-                <option value="">Входящие</option>
-            </select>
-        </div>
-
-        <div class="form__row">
-            <label class="form__label" for="date">Дата выполнения <sup>*</sup></label>
-
-            <input class="form__input form__input--date" type="text" name="date" id="date" value="" placeholder="Введите дату в формате ДД.ММ.ГГГГ">
-        </div>
-
-        <div class="form__row">
-            <label class="form__label" for="file">Файл</label>
-
-            <div class="form__input-file">
-                <input class="visually-hidden" type="file" name="preview" id="preview" value="">
-
-                <label class="button button--transparent" for="preview">
-                    <span>Выберите файл</span>
-                </label>
+        <h1 class="visually-hidden">Дела в порядке</h1>
+        <?=include_template('templates/header.php'); ?>
+        <div class="page-wrapper">
+            <div class="container container--with-sidebar">
+            <?=include_template('templates/main.php', ['project_list' => $project_list, 'tasks' => $tasks, 'days_until_deadline' => $days_until_deadline, 'date_deadline' => $date_deadline, 'tasks_filter' => $tasks_filter]); ?>
             </div>
         </div>
+        <?=include_template('templates/footer.php'); ?>
 
-        <div class="form__row form__row--controls">
-            <input class="button" type="submit" name="" value="Добавить">
+        <div class="modal" hidden>
+            <button class="modal__close" type="button" name="button">Закрыть</button>
+
+            <h2 class="modal__heading">Добавление задачи</h2>
+
+            <form class="form" class="" action="index.html" method="post">
+                <div class="form__row">
+                    <label class="form__label" for="name">Название <sup>*</sup></label>
+                    <input class="form__input" type="text" name="name" id="name" value="" placeholder="Введите название">
+                </div>
+
+                <div class="form__row">
+                    <label class="form__label" for="project">Проект <sup>*</sup></label>
+                    <select class="form__input form__input--select" name="project" id="project">
+                        <option value="">Входящие</option>
+                    </select>
+                </div>
+
+                <div class="form__row">
+                    <label class="form__label" for="date">Дата выполнения <sup>*</sup></label>
+                    <input class="form__input form__input--date" type="text" name="date" id="date" value="" placeholder="Введите дату в формате ДД.ММ.ГГГГ">
+                </div>
+
+                <div class="form__row">
+                    <label class="form__label" for="file">Файл</label>
+                    <div class="form__input-file">
+                        <input class="visually-hidden" type="file" name="preview" id="preview" value="">
+
+                        <label class="button button--transparent" for="preview">
+                            <span>Выберите файл</span>
+                        </label>
+                    </div>
+                </div>
+
+                <div class="form__row form__row--controls">
+                <input class="button" type="submit" name="" value="Добавить">
+                </div>
+            </form>                                                 
         </div>
-    </form>
-</div>
 
-<script type="text/javascript" src="js/script.js"></script>
-</body>
+        <script type="text/javascript" src="js/script.js"></script>
+    </body>
 </html>
