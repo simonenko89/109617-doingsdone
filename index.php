41467390463dccd4261eca0dd89c0e4ac7c55190
Igor Simonenko <simonenko89@gmail.com>
2017-05-10 22:59:43 +0300

    15 Аутентификация #2

--- index.php
+++ index.php
@@ -1,15 +1,10 @@
 <?php
-
-
+session_start();
 error_reporting(E_ALL);
 
 include 'functions.php';
 
-$days = rand(0, 3);
-$task_deadline_ts = strtotime("+" . $days . " day"); 
-$current_ts = time();
-$date_deadline = date("d.m.Y", $task_deadline_ts);
-$days_until_deadline = floor(($task_deadline_ts - $current_ts) / 86400);
+include 'logic.php';
 
 $project_list = ["Входящие", "Учеба", "Работа", "Домашние дела", "Авто"];
 $tasks = [
@@ -68,88 +63,6 @@ $users = [
         'password' => '$2y$10$2OxpEH7narYpkOT1H5cApezuzh10tZEEQ2axgFOaKW.55LxIJBgWW'
     ]
 ];
-
-
-
-if (isset($_POST['name']) && strlen($_POST['name']) > 0 && strlen($_POST['project']) > 0 && strlen($_POST['date']) > 0) {
-    $new_task = [
-        'task' => xss($_POST['name']),
-        'due_date' => xss($_POST['date']),
-        'project' => $_POST['project'],
-        'realized' => false
-    ];
-    
-    array_unshift($tasks, $new_task);
-}
-
-if (isset($_FILES['preview']) && is_uploaded_file($_FILES['preview']['tmp_name'])) {
-    move_uploaded_file($_FILES['preview']['tmp_name'], __DIR__.'/'.$_FILES['preview']['name']);
-}
-
-
-$tasks_filter = [];
-if (isset($_GET['project']) || (isset($_POST['send']) && !isset($_POST['name']))) {
-    if (!array_key_exists($_GET['project'], $project_list)) {
-        
-    header("HTTP/1.1 404 Not Found");
-    die("Страница не найдена");
-        
-    } else {
-        $tasks_filter = array_filtering($tasks, $project_list[$_GET['project']]);
-    }
-    
-} else {
-    $tasks_filter = $tasks;
-};
-
-
-//Форма логина, проверки
-$showLoginWin = false;
-
-if (isset($_GET['login']) || isset($_POST['email']) || isset($_POST['password'])) {
-    $showLoginWin = true;
-}
-
-$isValidEmail = true;
-$email = '';
-
-if (isset($_POST['email'])) {
-    if (strlen($_POST['email']) == 0) {
-        
-        $isValidEmail = false;
-        
-    } else {
-        
-        $isValidEmail = true;
-        $email = $_POST['email'];
-        
-    }
-}
-
-$isValidPass = (isset($_POST['password']) && strlen($_POST['password']) == 0) ? false : true;
-
-session_start();
-
-$isWrongPass = true;
-if (isset($_POST['password']) && isset($_POST['email'])) {
-    
-    $password = $_POST['password'];
-    
-    if ($user = searchUserByEmail($email, $users)) {
-
-        if (password_verify($password, $user['password'])) {
-            $_SESSION['user'] = $user;
-            
-            header("Location: /index.php");  
-            
-        } else {
-            
-            $isWrongPass = false;
-            
-        }
-    }
-}
-$username = (!empty($_SESSION)) ? $_SESSION['user']['name'] : '';
 
 ?>
 
