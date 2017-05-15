ed31a72996127746f4d59e9aca508c48be0f91bf
Igor Simonenko <simonenko89@gmail.com>
2017-05-15 22:03:33 +0300

    #17 Создание схемы БД личного проекта #2

--- schema.sql
+++ schema.sql
@@ -3,38 +3,32 @@ use todolist;
 
 create table projects (
     id int auto_increment primary key,
-    project char(128)
-);
-
-create table tasks (
-    id int auto_increment primary key,
-    create_dttm timestamp(6),
-    realized_dttm timestamp(6),
-    task char(128),
-    filename char(128),
-    due_dttm timestamp(6),
-    image_url char(128)
+    name varchar(255) not null
 );
 
 create table users (
     id int auto_increment primary key,
-    register_dttm timestamp(6),
-    email char(128),
-    name char(128),
-    pass_hash char(64),
-    phone char(12)
+    registered_dt timestamp default now(),
+    email varchar(255) not null,
+    name varchar(255) not null,
+    pass_hash varchar(255) not null,
+    phone varchar(255) null
 );
 
-create index project_i on projects(project);
-create index create_dttm_i on tasks(create_dttm);
+create table tasks (
+    id int auto_increment primary key,
+    project_id int null,
+    user_id int not null,
+    FOREIGN KEY (project_id) REFERENCES projects (id) on delete cascade,
+    FOREIGN KEY (user_id) REFERENCES users (id) on delete cascade,
+    created_dt timestamp default now(),
+    realized_dt timestamp null,
+    name varchar(255) not null,
+    filename varchar(255) null,
+    due_dt timestamp null,
+    image_url varchar(255) null
+);
 
-create index realized_dttm_i on tasks(realized_dttm);
-create index task_i on tasks(task);
-create index filename_i on tasks(filename);
-create index due_dttm_i on tasks(due_dttm);
-create index image_url_i on tasks(image_url);
-
-create index register_dttm_i on users(register_dttm);
+create index project_id_i on tasks(project_id);
+create index user_id_i on tasks(user_id);
 create unique index email_i on users(email);
-create index name_i on users(name);
-create unique index phone_i on users(phone);
