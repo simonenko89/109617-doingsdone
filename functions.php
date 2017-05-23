7b9ff2f90e87583fe88c5d9a1d5a537a581a0038
Igor Simonenko <simonenko89@gmail.com>
2017-05-23 21:34:10 +0300

    #21 Функции для работы с MySql #1

--- functions.php
+++ functions.php
@@ -1,5 +1,79 @@
 <?php
 
+require_once 'mysql_helper.php';
+
+function db_select($link, string $sql, array $data = []): array 
+{
+    $result = [];
+    
+    if ($link == false) {
+        $result = ["Ошибка подключения: " . mysqli_connect_error()];
+    }
+    
+    $stmt = db_get_prepare_stmt($link, $sql, $data);
+    
+    return $stmt;    
+}
+
+
+function db_insert(mixed $link, string $sql, array $data = []): mixed
+{
+    $stmt = db_get_prepare_stmt($link, $sql, $data);
+    
+    $query_result = mysqli_query($link, $stmt);
+    
+    if ($query_result == false) {
+       $result = false; 
+        
+    } else {
+        
+        $result = mysqli_insert_id($link);
+    }
+    
+    return $result;    
+}
+
+
+function db_update(mixed $link, string $table_name, array $update_data = [], array $where_data = []): mixed
+{
+    $sql_update = '';
+    $sql_where ='';
+    
+    foreach ($update_data as $key => $value) {
+        $sql_update .= "$key = $value";
+    }
+    $sql_update = substr($sql_update, 0, length($sql_update)-2);
+    
+    foreach ($where_data as $key => $value) {
+        $sql_where .= "$key = $value";
+    }
+    $sql_where = substr($sql_where, 0, length($sql_where)-2);
+    
+    if (length($sql_update) == 0) {
+        $result = false;
+    } else if (length($sql_update) > 0 and length($sql_where) == 0){
+        $sql = "update $table_name set $sql_update";
+    } else {
+        $sql = "update $table_name set $sql_update where $sql_where";
+    }
+    
+    $stmt = db_get_prepare_stmt($link, $sql, $update_data);
+    
+    $query_result = mysqli_query($link, $stmt);
+    
+    if ($query_result == false) {
+       $result = false; 
+        
+    } else {
+        
+        $result = mysqli_affected_rows($link);
+    }
+    
+    return $result; 
+}
+
+
+
 function include_template(string $template, array $params = []): string
 {
     if (!file_exists($template)) {
